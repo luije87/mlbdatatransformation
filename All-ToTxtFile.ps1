ade82a8e6e92542bd7e1e0252d645abd9544a7e3
luije87 <luije87@gmail.com>
2017-11-29 21:49:26 -0500

    2
    2

--- All-ToTxtFile.ps1
+++ All-ToTxtFile.ps1
@@ -12,6 +12,8 @@ function Rosters-ToTxtFile {
 	New-Item -path "$Path\$Year\PLAYERS.txt" -type file -Force
 	$content = Get-Content "$Path\*$Year.ROS"	
 	$content | ForEach-Object { ($_ + "," + $year) } | Set-Content -Path "$Path\$Year\PLAYERS.txt"
+	#Set-Content -Value $content -Path "$Path\$Year\PLAYERS.txt"	
+	
 }
 
 function Teams-ToTxtFile {
@@ -52,7 +54,8 @@ function GamesExt-ToTxtFile {
 	)
 	
 	$cwgamepath = "$Path\cwgame.exe"
-	& $cwgamepath -x 0-94 -f 83,0 -y $Year "$Year*.ev*" >$Year\GAMES-EXT.txt
+	#THIS LINE MEANS 0-94 COLUMNS FROM EXTENSION AND 0 AND 1 FROM NORMAL
+	& $cwgamepath -x 0-94 -f 0,1,2 -y $Year "$Year*.ev*" >$Year\GAMES-EXT.txt
 	(Get-Content "$Path\$Year\GAMES-EXT.txt") | foreach {$_ -replace '"'} | Set-Content "$Path\$Year\GAMES-EXT.txt"
 }
 
@@ -93,14 +96,14 @@ function Run {
 	)
 	New-Item -ItemType Directory -Path "$scriptDirectory\$Year" -Force
 	Rosters-ToTxtFile -Path $scriptDirectory -Year $Year
-	#Teams-ToTxtFile -Path $scriptDirectory -Year $Year
-	#Games-ToTxtFile -Path $scriptDirectory -Year $Year
-	#GamesExt-ToTxtFile -Path $scriptDirectory -Year $Year
-	#Events-ToTxtFile -Path $scriptDirectory -Year $Year
-	#EventsExt-ToTxtFile -Path $scriptDirectory -Year $Year			
+	Teams-ToTxtFile -Path $scriptDirectory -Year $Year
+	Games-ToTxtFile -Path $scriptDirectory -Year $Year
+	GamesExt-ToTxtFile -Path $scriptDirectory -Year $Year
+	Events-ToTxtFile -Path $scriptDirectory -Year $Year
+	EventsExt-ToTxtFile -Path $scriptDirectory -Year $Year			
 }
 
-for ($i = 2000; $i -lt 2001; $i++)
+for ($i = 2000; $i -lt 2017; $i++)
 { 
 	Run -Year $i
 }
